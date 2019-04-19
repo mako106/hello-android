93ce1bb7d4869986c044478430c78477de3b7b80
Your NameMako <chienthien98na@gmail.com>
2019-04-19 19:47:59 +0700

    my location

--- app/src/main/java/com/example/bottomnavigationdemo/Activity/MainActivity.java
+++ app/src/main/java/com/example/bottomnavigationdemo/Activity/MainActivity.java
@@ -1,30 +1,108 @@
 package com.example.bottomnavigationdemo.Activity;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.example.bottomnavigationdemo.API.RetrofitClient;
 import com.example.bottomnavigationdemo.Adapter.BottomNavigationBehavior;
+import com.example.bottomnavigationdemo.Entity.TokenFirebase;
 import com.example.bottomnavigationdemo.Fragment.FeedBackFragment;
 import com.example.bottomnavigationdemo.Fragment.HomeFragment;
 import com.example.bottomnavigationdemo.Fragment.InfoTrafficFragment;
 import com.example.bottomnavigationdemo.Fragment.NotiFragment;
 import com.example.bottomnavigationdemo.Fragment.ProfileFragment;
+import com.example.bottomnavigationdemo.Pojo.registerResult;
 import com.example.bottomnavigationdemo.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
-public class MainActivity extends AppCompatActivity {
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity   {
+
+    private static final String TAG = "MyFirebaseMsgService";
+    private static final String TAG_Google = "GoogleAPI";
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        FirebaseApp.initializeApp(this);
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "getInstanceId failed", task.getException());
+                            return;
+                        }
+
+                        // Get new Instance ID token
+                        String token = task.getResult().getToken();
+
+                        // Log and toast
+                        String msg = getString(R.string.msg_token_fmt, token);
+                        Log.d(TAG, msg);
+                        TokenFirebase tokenFirebase = new TokenFirebase();
+                        tokenFirebase.setToken(token);
+                        Call<registerResult> call = RetrofitClient.getInstance().getApi().saveToken(tokenFirebase);
+                        call.enqueue(new Callback<registerResult>() {
+                            @Override
+                            public void onResponse(Call<registerResult> call, Response<registerResult> response) {
+                                registerResult s = response.body();
+                                Log.d("Response", response.message());
+                                if (s == null) {
+                                    Log.d("Error", "save fail");
+                                    return;
+                                }
+                                Log.d("Save", "Success");
+                            }
+
+                            @Override
+                            public void onFailure(Call<registerResult> call, Throwable t) {
+                                Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+
+                            }
+                        });
+                    }
+                });
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
@@ -36,6 +114,10 @@ public class MainActivity extends AppCompatActivity {
         // load the store fragment by default
         loadFragment(new NotiFragment());
     }
+
+
+
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -82,5 +164,7 @@ public class MainActivity extends AppCompatActivity {
         transaction.addToBackStack(null);
         transaction.commit();
     }
+
+
 
 }
